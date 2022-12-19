2081a0c633ddd4692976d254a358d21fb6d94de9
Aamir Shaikh <aamirshaikh3232@outlook.com>
2022-12-19 20:21:56 +0530

    first commit

---
+++ src/main/java/com/demo/fullstackspringreact/FullStackSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.demo.fullstackspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackSpringReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/fullstackspringreact/FullStackSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.fullstackspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
